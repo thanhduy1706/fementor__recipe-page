a6cfec211bf76c882baffa20b74341ab75e3b1e8
Nguyen Duy <thanhduy1706@gmail.com>
2024-06-08 13:48:41 +0700

    Initial config

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'nutmeg': 'hsl(14, 45%, 36%)',
+        'dark-raspberry': 'hsl(332, 51%, 32%)',
+        'white': 'hsl(0, 0%, 100%)',
+        'rose-white': 'hsl(330, 100%, 98%)',
+        'eggshell': 'hsl(30, 54%, 90%)',
+        'light-grey': 'hsl(30, 18%, 87%)',
+        'wenge-brown': 'hsl(30, 10%, 34%)',
+        'dark-charcoal': 'hsl(24, 5%, 18%)',
+      },
+      fontFamily: {
+        'young-serif': ['Young Serif', 'serif'],
+        'outfit': ['Outfit', 'sans-serif'],
+      },
+      fontWeight: {
+        '400': 400,
+        '600': 600,
+        '700': 700,
+      },
+      screens: {
+        sm: "375px",
+        lg: "1440px",
+      },
+    },
+  },
+  plugins: [],
+}
